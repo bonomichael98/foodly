acd74d9a35a1818e77054d54e7d1d23be3c301aa
Josiah Douglass <josiahdouglass64@gmail.com>
2022-05-12 20:05:01 -0400

    added homeRoutes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const session = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT|| 3001;
@@ -16,16 +16,16 @@ const sesh = {
 
 app.use(session(sesh));
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
+// app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
