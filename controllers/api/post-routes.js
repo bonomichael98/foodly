40ce01b4abf37942f1a7701973040c83fb83b3c2
Jay Ford <jay.ford45@gmail.com>
2022-05-17 18:53:40 -0400

    added routes for posts and users

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -41,3 +41,5 @@ router.put('/:id', (req, res) => {
 
     })
 })
+
+module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,3 @@
 DROP DATABASE IF EXISTS foodly_db;
 
-CREATE DATABASE museum_db;
+CREATE DATABASE foodly_db;
