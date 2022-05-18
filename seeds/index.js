b4e710a07a775ef04e124f3e795a67d1e87ef7dd
michael <shinlin09@gmail.com>
2022-05-17 20:20:45 -0400

    posts fix array issue

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,13 @@
 const sequelize = require('../config/connection');
+const recipesList = require('../seeds/posts')
+const seedUsers = require('../seeds/user')
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
+  await seedUsers();
 
+  await recipesList();
 
   process.exit(0);
 };
