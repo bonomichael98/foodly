dd1f60478480f985f2194f20c414e651bf7d7c4d
Jay Ford <jay.ford45@gmail.com>
2022-05-24 17:42:28 -0400

    updates to sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -68,14 +68,12 @@ router.get('/post/:id', (req, res) => {
   });
 })
 
-// Login route
+
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect to the homepage
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
-  // Otherwise, render the 'login' template
   res.render('login');
 });
 
--- server.js
+++ server.js
@@ -6,16 +6,21 @@ const exphbs = require('express-handlebars');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT|| 3001;
 
 const sesh = {
-    secret: 'this is a secret',
+    secret: 'Super secret secret',
+    cookie: {},
     resave: false,
-    saveUninitialized: true
-};
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.use(session(sesh));
 
